a2285541bf37b4adf22487b822faefa40a509a25
Fabiano Cardoso <sl.biano28@gmail.com>
2022-01-28 19:07:52 -0300

    Finish project ComposicaoExer

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,8 +1,11 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Client {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	private String name;
 	private String email;
@@ -32,6 +35,11 @@ public class Client {
 	
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return name + " (" + sdf.format(birthDate) + ") - " + email;
 	}
 	
 }
